987ce0e689103adfe94ccea2d867dcfecea9e95f
TomTheDark <tomthedark@gmail.com>
2026-02-05 16:25:07 +0100

    feat(TomTheDark): Exercice fait [15][DONE] space-age

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, seconds) => {
+  const orbitalPeriods = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  };
+
+  if (!(planet in orbitalPeriods)) {
+    throw new Error("not a planet");
+  }
+
+  const earthYearInSeconds = 31557600;
+  const ageInEarthYears = seconds / earthYearInSeconds / orbitalPeriods[planet];
+  
+  return Math.round(ageInEarthYears * 100) / 100;
 };
